18274aa419e74c32e7d77174776ddcfff24c94e0
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-05-19 09:57:44 +0200

    Initial Commit

---
+++ MyContacts/MyContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsApp.swift
+//  MyContacts
+//
+//  Created by Hossein Sharifi on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
